2902148fbe85b976efa696a1e14c68661f94de41
Wahab901278 <abdulwahab901278@gmail.com>
2025-06-02 12:00:41 +0500

    Fixed paths issues

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -3,15 +3,16 @@ import pandas as pd
 import os
 from src.exception import CustomException
 from src.utils import load_object
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 class PredictPipeline:
     def __init__(self):
         pass
 
     def predict(self,features):
         try:
-            model_path=os.path.join('artifacts','model.pkl')
-            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            
+            model_path = os.path.join(BASE_DIR, 'artifacts', 'model.pkl')
+            preprocessor_path = os.path.join(BASE_DIR, 'artifacts', 'preprocessor.pkl')
             print("Before loading")
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
